b104611b6ab193eb6e64ccb7689849d39d5d4558
Robin Matz <robin.s.matz@gmail.com>
2024-07-09 08:06:51 +0200

    create docker volume for logs

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
+import logging
 import os
-from flask import Flask
+
+from flask import Flask, request
+
 
 app = Flask(__name__)
+os.makedirs("logs", exist_ok=True)
+logging.basicConfig(
+    filename="logs/application.log", encoding="utf-8", level=logging.DEBUG
+)
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, world</p>"
 
+
 @app.route("/stage")
 def get_stage():
     stage = os.getenv("APP_STAGE", "dev")
+    app.logger.info(f"Receiving request: {request}")
     return {"stage": stage}
+
 
 @app.route("/env_values")
 def env_values():
     environment_variables = os.environ.items()
-    return {key: value for key, value in environment_variables if key.startswith("APP_")}
+    return {
+        key: value for key, value in environment_variables if key.startswith("APP_")
+    }
